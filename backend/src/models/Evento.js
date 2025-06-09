344bf72911260a41cd16a640ade56b618556649b
ekirmen <42483861+ekirmen@users.noreply.github.com>
2025-06-09 15:23:21 -0400

    feat: store full event data

--- backend/src/models/Evento.js
+++ backend/src/models/Evento.js
@@ -6,12 +6,126 @@ const eventoSchema = new mongoose.Schema({
   slug: { type: String, unique: true },
   fecha: { type: Date, required: true }, // Add fecha field
   sector: { type: String },
+  descripcion: { type: String },
   descripcionHTML: { type: String },
   resumenDescripcion: { type: String },
   videoURL: { type: String },
   recinto: { type: mongoose.Schema.Types.ObjectId, ref: 'Recinto', required: true },
   sala: { type: mongoose.Schema.Types.ObjectId, ref: 'Sala', required: true },
   tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }],
+  modoVenta: { type: String },
+  forzarRegistro: { type: Boolean, default: false },
+  maxTicketsCompra: { type: Number, default: 10 },
+  maxEntradasEmail: { type: Number, default: 0 },
+  mostrarPrecioMinimo: { type: Boolean, default: false },
+  mostrarPrecioDesdeConComision: { type: Boolean, default: false },
+  mostrarPrecioDesdeSinComision: { type: Boolean, default: false },
+  eventoSinLanding: { type: Boolean, default: false },
+  forzarFlujoPromociones: { type: Boolean, default: false },
+  eventoSinFecha: { type: Boolean, default: false },
+  datosComprador: {
+    nombre: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    email: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    telefono: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    rut: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    numeroIdentificacionFiscal: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    direccion: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    nombreFonetico: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    apellidosFoneticos: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    idioma: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    fechaNacimiento: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    sexo: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    empresa: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    departamento: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    cargoEmpresa: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    matricula: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    twitter: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    facebook: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    youtube: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    tiktok: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    snapchat: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    instagram: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    contactoEmergencia: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    },
+    nacionalidad: {
+      solicitado: { type: Boolean, default: false },
+      obligatorio: { type: Boolean, default: false }
+    }
+  },
+  datosBoleto: {
+    rutTitular: { type: Boolean, default: false },
+    idPasaporte: { type: Boolean, default: false },
+    nombreTitular: { type: Boolean, default: false },
+    verificarEmail: { type: Boolean, default: false },
+    verificacionEmail: { type: Boolean, default: false },
+    pregunta1: { type: Boolean, default: false },
+    pregunta2: { type: Boolean, default: false }
+  },
   imagenes: {
     banner: String,
     obraImagen: String,
@@ -47,6 +161,7 @@ const eventoSchema = new mongoose.Schema({
       mostrar: { type: Boolean, default: false },
       texto: { type: String }
     },
+    habilitarMetodosPago: { type: Boolean, default: false },
     metodosPagoPermitidos: {
       type: [String],
       default: []
