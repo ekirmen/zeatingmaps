698a12672ba9f85a0e9aeac544b3b6690f47bb01
ekirmen <42483861+ekirmen@users.noreply.github.com>
2025-06-10 11:18:28 -0400

    chore: store uploads inside src

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const destination = path.join(__dirname, '..', 'public', 'uploads', 'eventos', 'espectaculo');
+const destination = path.join(__dirname, '..', 'uploads', 'eventos', 'espectaculo');
 
 // Crear el directorio si no existe
 if (!fs.existsSync(destination)) {
