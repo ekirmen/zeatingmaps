138289a767148799a51ad73e9584111ad0d75b3e
ekirmen <42483861+ekirmen@users.noreply.github.com>
2025-06-11 09:15:58 -0400

    fix: apply Ant Design styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Asegúrate de que aquí están las directivas de Tailwind
+import 'antd/dist/reset.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
