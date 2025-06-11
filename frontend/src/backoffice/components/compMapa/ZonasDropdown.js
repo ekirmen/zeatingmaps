3d110a519bb7cb5d8f903154fdacc5da6ae66b9d
ekirmen <42483861+ekirmen@users.noreply.github.com>
2025-06-11 15:31:43 -0400

    Show seat counts per zone in map editor

--- frontend/src/backoffice/components/compMapa/ZonasDropdown.js
+++ frontend/src/backoffice/components/compMapa/ZonasDropdown.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const ZonasDropdown = ({ zonas, selectedZoneId, onChange }) => {
+const ZonasDropdown = ({ zonas, zoneSeatCounts = {}, selectedZoneId, onChange }) => {
   return (
     <select value={selectedZoneId || ''} onChange={(e) => onChange(e.target.value)}>
       <option value=''>Seleccionar zona</option>
       {zonas.map((zona) => (
         <option key={zona._id} value={zona._id}>
-          {zona.nombre}
+          {zona.nombre} ({zoneSeatCounts[zona._id] || 0})
         </option>
       ))}
     </select>
