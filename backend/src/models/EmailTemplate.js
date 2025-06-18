3801b8f7ace31e1f258f972d474d570144eb28bc
ekirmen <redakai1994@gmail.com>
2025-06-18 11:08:40 -0400

    añadiento test de frontend src pages test.js

--- backend/src/models/EmailTemplate.js
+++ backend/src/models/EmailTemplate.js
@@ -1,11 +1,1 @@
-import mongoose from 'mongoose';
-
-const emailTemplateSchema = new mongoose.Schema({
-  type: { type: String, required: true, unique: true },
-  subject: { type: String, required: true },
-  body: { type: String, required: true }
-});
-
-const EmailTemplate = mongoose.model('EmailTemplate', emailTemplateSchema);
-
-export default EmailTemplate;
+c
