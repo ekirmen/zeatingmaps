249eb7697e81412c1a5d1fdc52d23b61a76278a9
ekirmen <redakai1994@gmail.com>
2025-06-18 20:30:24 -0400

    🔥 Elimina backend y migración completa a Supabase 2

--- backend/src/utils/slugify.js
+++
@@ -1,9 +0,0 @@
-export const slugify = (text = '') => {
-  return text
-    .toString()
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    .toLowerCase()
-    .replace(/[^a-z0-9]+/g, '-')
-    .replace(/^-+|-+$/g, '');
-};
