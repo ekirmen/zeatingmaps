af461ce11f9e041068c310d30c4125c1077e0b30
ekirmen <42483861+ekirmen@users.noreply.github.com>
2025-07-03 10:04:36 -0400

    Move seat selection page under store and update imports

--- src/store/pages/SeatSelectionPage.jsx
+++ src/store/pages/SeatSelectionPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SeatMap from '../components/SeatMap';
+import SeatMap from '../../components/SeatMap';
 import { useParams } from 'react-router-dom';
 
 const SeatSelectionPage = () => {
