71bc717b5f6ce8122cee58d0da9ba3309dfe696a
ekirmen <redakai1994@gmail.com>
2025-08-26 14:38:51 -0400

    tickera 4.1.1

--- src/components/VercelSpeedInsights.jsx
+++ src/components/VercelSpeedInsights.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 // Componente wrapper para Vercel Speed Insights
 const VercelSpeedInsights = () => {
