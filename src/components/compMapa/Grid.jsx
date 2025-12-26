668a2442cc7dd8ca9197b49e975b0d47172d50b6
ekirmen <redakai1994@gmail.com>
2025-12-26 14:45:03 -0400

    fix: update GridCore import to jsx extension

--- src/components/compMapa/Grid.jsx
+++ src/components/compMapa/Grid.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import LoadOnVisible from '../LoadOnVisible';
 
 export default function Grid(props) {
-  return <LoadOnVisible loader={() => import('./GridCore.js')} loaderProps={props} />;
+  return <LoadOnVisible loader={() => import('./GridCore.jsx')} loaderProps={props} />;
 }
